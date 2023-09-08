deb4d1749d527481ff9cc0e4c4fff8d906f0458e
2102819372 <81741234+2102819372@users.noreply.github.com>
2023-09-08 10:35:53 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <First v-if="show" />
+    <button @click="isShow">展示</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, Ref } from 'vue';
+import First from './components/First.vue';
+const show: Ref = ref(true);
+const isShow = () => {
+  show.value = !show.value;
+};
+</script>
+<style scoped></style>
---
+++ src/components/First.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    ref="chartRef"
+    style="height: 500px; width: 500px"
+    class="first-container"
+  ></div>
+</template>
+<script setup lang="ts">
+import * as echarts from 'echarts';
+import { onMounted, ref, Ref, onBeforeUnmount } from 'vue';
+const option = {
+  tooltip: {},
+  legend: {},
+  xAxis: {
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'Sale',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20, 4],
+    },
+  ],
+};
+const chartRef: Ref = ref(null);
+onMounted(() => {
+  window.addEventListener('resize', () => {
+    chart.resize();
+  });
+  const mycharts = echarts.init(chartRef.value);
+  option && mycharts.setOption(option);
+});
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', () => {
+    chart.resize();
+    console.log('remove');
+  });
+});
+</script>
+<style scoped></style>
