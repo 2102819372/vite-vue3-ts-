fa1ffcc2bedf2b67d3e36118740229af5c92cec4
2102819372 <81741234+2102819372@users.noreply.github.com>
2023-09-11 11:12:51 +0800

    3 files modified

--- src/components/Konva.vue
+++ src/components/Konva.vue
@@ -1,5 +1,93 @@
 <template>
-  <div id="konva" style="height: 50vh; width: 90vw"></div>
+  <div id="konva" ref="konvaRef" style="height: 50vh; width: 100vw"></div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// const appDiv: HTMLDivElement = document.querySelector('#id');
+import Konva from 'konva';
+import { ref, Ref, onMounted } from 'vue';
+const konvaRef: Ref = ref(null);
+function iframe() {
+  const div = document.createElement('div');
+  div.classList.add('model');
+  const iframe = document.createElement('iframe');
+  const url = document.location.href;
+  iframe.src = url + '/api';
+  div.appendChild(iframe);
+  document.body.appendChild(div);
+}
+function init(appDiv) {
+  const { clientWidth, clientHeight } = appDiv;
+  let stage: Konva.Stage = new Konva.Stage({
+    container: appDiv,
+    width: clientWidth,
+    height: clientHeight,
+    draggable: true,
+  });
+  const scaleBy = 1.1;
+  stage.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    const oldScale = stage.scaleX();
+    const pointer = stage.getPointerPosition() as Konva.Vector2d;
+    const mousePointTo = {
+      x: (pointer.x - stage.x()) / oldScale,
+      y: (pointer.y - stage.y()) / oldScale,
+    };
+    const direction = e.wheelDelta > 0 ? 1 : -1;
+    const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+    stage.scale({ x: newScale, y: newScale });
+    const newPos = {
+      x: pointer.x - mousePointTo.x * newScale,
+      y: pointer.y - mousePointTo.y * newScale,
+    };
+    stage.position(newPos);
+  });
+  const layerbg: Konva.Layer = new Konva.Layer();
+  const layerMark: Konva.Layer = new Konva.Layer();
+  //添加圆形
+  var rect = new Konva.Circle({
+    x: 50,
+    y: 50,
+    width: 50,
+    height: 50,
+    fill: 'red',
+    draggable: true,
+  });
+  //添加点击事件
+  rect.on('click', () => {
+    iframe();
+  });
+  //注册到layer
+  layerMark.add(rect);
+  //绘制图片
+  const originImg = new Image();
+  originImg.src = 'http://shanhe.kim/api/wz/bing.php?rand=true';
+  originImg.onload = () => {
+    // 绘制图片
+    const img = new Konva.Image({
+      x: 0,
+      y: 0,
+      image: originImg,
+    });
+    const w = img.width();
+    const h = img.height();
+    const sw = stage.width();
+    const sh = stage.height();
+    const wr = sw / w;
+    const hr = sh / h;
+    const s = Math.min(wr, hr);
+    img.scale({ x: s, y: s });
+    img.position({
+      x: sw / 2 - (w * s) / 2,
+      y: sh / 2 - (h * s) / 2,
+    });
+    layerbg.add(img);
+  };
+
+  stage.add(layerbg);
+  stage.add(layerMark);
+}
+onMounted(() => {
+  init(konvaRef.value);
+});
+</script>
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      // 代理规则
+      '/api': {
+        target: 'http://117.172.29.137:10087/', // 目标服务器的地址
+        changeOrigin: true, // 开启代理时，在服务器端会虚拟一个服务器接收请求，然后在这个服务器上转发请求，而不是直接对目标服务器发起请求
+        rewrite: (path) => path.replace(/^\/api/, ''), // 路径重写，移除路径中的/api
+      },
+    },
+  },
+});
